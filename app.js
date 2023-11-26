d943c772bd532de799de2964a18cec19f12a6190
bij51247 <kmrttm247810@gmail.com>
2023-11-26 16:19:54 +0900

    ピンを置く前

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ new Vue({
     embeddedMax: 0,
     uploadedCurrent: 0,
     uploadedMax: 0,
+    embeddedVideoSrc: 'example.mp4', // 埋め込み動画のデフォルトソース
+    embeddedStart: 0,
   },
   methods: {
     handleFileChange(event) {
@@ -54,5 +56,43 @@ new Vue({
         videoPlayer.currentTime = value;
       }
     },
+    handleEmbeddedFileChange(event) {
+      const file = event.target.files[0];
+      if (file) {
+        this.embeddedVideoSrc = URL.createObjectURL(file);
+        const embeddedVideoPlayer = this.$refs.embeddedVideo;
+        if (embeddedVideoPlayer) {
+          embeddedVideoPlayer.load();
+        }
+      }
+    },
+    setStartingPosition(event, videoType) {
+      const value = parseFloat(event.target.value);
+      this[`${videoType}Start`] = value;
+      // Make the video start from the pin's position when played
+      const videoPlayer = this.$refs[`${videoType}Video`];
+      if (videoPlayer) {
+        videoPlayer.currentTime = value;
+      }
+
+      // Optionally, if you want the video to start playing immediately from the pin position
+      if (!videoPlayer.paused) {
+        videoPlayer.play();
+      }
+    },
+
+    // Use this method to make the pin draggable
+    makePinInteractive(videoType) {
+      const pin = this.$refs[`${videoType}Pin`];
+      if (pin) {
+        pin.classList.add('interactive');
+      }
+    }
+  },
+  mounted() {
+    this.makePinInteractive('embedded');
+    // If you have other video types or uploaded videos, call the method for them as well
   }
 });
+
+
